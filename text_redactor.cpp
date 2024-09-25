1044b0a5db54f4050c2513c70ab5eba2ef6a3c55
Wraxuz <112343075+Wraxuz@users.noreply.github.com>
2024-09-25 13:51:13 +0300

    Update text_redactor.cpp

--- text_redactor.cpp
+++ text_redactor.cpp
@@ -1,160 +1,63 @@
-// Напишите код упрощенного текстового редактора здесь#include <iostream>
-#include <string>
-#include <deque>
-#include <algorithm>
-#include <iterator>
-#include <list>
 #include <iostream>
+#include <list>
+#include <string>
+
+
 
 using namespace std;
 
 class Editor {
 public:
-    Editor() : cursor(0), it(text.end()) {}
-    
+    Editor()
+            : pos(text.end()) {
+    }
+
     void Left() {
-        if (cursor > 0){
-            --it;
-            --cursor;
-        }
-        //cout << *it << endl;
+        pos = Advance(pos, -1);
     }
 
     void Right() {
-        //cout << cursor << " " << *it << endl;
-        if (cursor < text.size()){
-            ++it;
-            ++cursor;
-        }
-        //cout << *it << " "s << cursor << endl;
+        pos = Advance(pos, 1);
     }
 
     void Insert(char token) {
-        if (cursor == text.size()){
-            //cout << token << endl;
-            text.push_back(token);
-            it = text.end();
-        }
-        else{
-            text.insert(it--, token);
-        }
-        ++cursor;
-        //cout << cursor << endl;
-        
+        text.insert(pos, token);
     }
 
     void Cut(size_t tokens = 1) {
-        list<char>::iterator ne = it;
-        buffer.clear();
-        if (cursor + tokens < text.size()){
-            size_t i = 0;
-            while (i < tokens){
-                ++ne;
-                i += 1;
-            }
-        }
-        else{
-            ne = text.end();
-        }
-        buffer.insert(buffer.begin(), it, ne);
-        text.erase(it, ne);
-        it = ne;
+        auto pos2 = Advance(pos, tokens);
+        buffer.assign(pos, pos2);
+        pos = text.erase(pos, pos2);
     }
 
     void Copy(size_t tokens = 1) {
-        list<char>::iterator ne = it;
-        buffer.clear();
-        if (cursor + tokens < text.size()){
-            size_t i = 0;
-            while (i < tokens){
-                ++ne;
-                ++cursor;
-                i += 1;
-            }
-        }
-        else{
-            ne = text.end();
-            cursor = text.size();
-        }
-        buffer.insert(buffer.begin(), it, ne);
+        auto pos2 = Advance(pos, tokens);
+        buffer.assign(pos, pos2);
     }
 
     void Paste() {
-        if (!buffer.empty()) {
-            //cout << 1 << endl;
-            /*if (it == text.end()){
-                text.push_back(' ');
-            }
-            else{
-                text.insert(it, ' ');
-
-            }*/
-            //it++;
-            text.insert(it, buffer.begin(), buffer.end());
-            cursor += buffer.size();
-            //size_t i = 0;
-            //while (i < buffer.size()){
-                //++it;
-                //++i;
-            //}
-        }
+        text.insert(pos, buffer.begin(), buffer.end());
     }
 
     string GetText() const {
-        string ans = ""s;
-        for (auto i : text){
-            ans += i;
-        }
-        //cout << cursor << endl;
-        //cout << *it << endl;
-        
-        return ans;
+        return {text.begin(), text.end()};
     }
 
 private:
-    list<char> text = {};
-    deque<char> buffer;
-    size_t cursor;
-    list<char>::iterator it;
+    using Iterator = list<char>::iterator;
+    list<char> text;
+    list<char> buffer;
+    Iterator pos;
+
+    Iterator Advance(Iterator it, int steps) const {
+        while (steps > 0 && it != text.end()) {
+            ++it;
+            --steps;
+        }
+        while (steps < 0 && it != text.begin()) {
+            --it;
+            ++steps;
+        }
+        return it;
+    }
 };
-
-int main() {
-    Editor editor;
-    const string text = "hello, world"s;
-    for (char c : text) {
-        editor.Insert(c);
-    }
-    //cout << editor.GetText() << endl;
-    // Текущее состояние редактора: `hello, world|`
-    for (size_t i = 0; i < text.size() + 1; ++i) {
-        editor.Left();
-    }
-    // Текущее состояние редактора: `|hello, world`
-    //cout << editor.GetText() << endl;
-    editor.Cut(7);
-    cout << editor.GetText() << endl;
-    // Текущее состояние редактора: `|world`
-    // в буфере обмена находится текст `hello, `
-    for (size_t i = 0; i < 5; ++i) {
-        editor.Right();
-    }
-    
-    // Текущее состояние редактора: `world|`
-    //cout << editor.GetText() << endl;
-    editor.Insert(',');
-    cout << editor.GetText() << endl;
-    editor.Insert(' ');
-    cout<< editor.GetText() << endl;
-
-    // Текущее состояние редактора: `world, |`
-    editor.Paste();
-    //cout << editor.GetText() << endl;
-    // Текущее состояние редактора: `world, hello, |`
-    editor.Left();
-    editor.Left();
-    //Текущее состояние редактора: `world, hello|, `
-    editor.Cut(3);  // Будут вырезаны 2 символа
-    // Текущее состояние редактора: `world, hello|`
-    cout << editor.GetText();
-    return 0;
-}
